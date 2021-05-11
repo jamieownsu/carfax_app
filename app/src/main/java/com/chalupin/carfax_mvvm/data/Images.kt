7a9076f9d32feacd6611d15b7ceba4fdfe15e534
Jamie <jamie.a.otoole@gmail.com>
2021-05-11 10:19:12 -0400

    cleanup

--- app/src/main/java/com/chalupin/carfax_mvvm/data/Images.kt
+++ app/src/main/java/com/chalupin/carfax_mvvm/data/Images.kt
@@ -1,6 +1,5 @@
 package com.chalupin.carfax_mvvm.data
 
-import androidx.room.Embedded
 import java.io.Serializable
 
-data class Images(@Embedded(prefix = "frst")val firstPhoto: FirstPhoto) : Serializable
+data class Images(val firstPhoto: FirstPhoto) : Serializable
